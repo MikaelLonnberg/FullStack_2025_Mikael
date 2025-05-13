e8e4a27e5f605a787c37963a1e3016ca477436f6
MikaelLonnberg <mikael.lonnberg@outlook.com>
2025-05-13 19:25:30 +0300

    Progressing the course

--- Project_Next.js/nextjs-dashboard/app/ui/dashboard/cards.tsx
+++ Project_Next.js/nextjs-dashboard/app/ui/dashboard/cards.tsx
@@ -1,3 +1,4 @@
+import { fetchCardData } from '@/app/lib/data';
 import {
   BanknotesIcon,
   ClockIcon,
@@ -5,6 +6,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,10 +16,15 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+   const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-      { /*
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
@@ -25,7 +32,7 @@ export default async function CardWrapper() {
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
